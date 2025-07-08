dcb4f80d3fbd2f0bfd84fd7b1c3c0a25e49fbbd8
misyaguziya <53165965+misyaguziya@users.noreply.github.com>
2025-07-09 06:10:57 +0900

    feat: pyinstallerの出力名をVTVV-ConnectorからVRCT-TTSに変更

--- build.bat
+++ build.bat
@@ -3,5 +3,5 @@ call .venv/Scripts/activate
 pip install -r requirements.txt
 if exist build rmdir /s /q build
 if exist dist rmdir /s /q dist
-pyinstaller --onedir --clean --name VTVV-Connector --noconsole main.py --add-data "./fonts:fonts"
+pyinstaller --onedir --clean --name VRCT-TTS --noconsole main.py --add-data "./fonts:fonts"
 if exist .venv rmdir /s /q .venv
